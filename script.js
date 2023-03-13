ea71aab7ebfba9fcdffc81c9d1f255e2998fa634
soosanna <84853503+soosanna@users.noreply.github.com>
2023-03-13 12:24:18 +0530

    demo dom file

---
+++ script.js
@@ -0,0 +1,22 @@
+/*var ele=document.createElement("div");
+ele.innerHTML="this is div";
+ele.setAttribute("class","main");
+document.body.append(ele);
+console.log(ele);
+
+var ele1=document.createElement("div");
+ele1.innerHTML="this is container"
+
+*/
+function foo(){
+var res1=document.getElementById("Firstname").value;
+console.log(res1);    
+var res2=document.getElementById("Secoundname").value;
+console.log(res2);    
+var res3=document.getElementById("lastname").value;
+console.log(res3);    
+var res4=document.getElementById("main").value;
+console.log(res2);
+var res=document.getElementById("password").value;
+    console.log(res);    
+}
